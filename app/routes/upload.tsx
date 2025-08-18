cf1f02ceeecfc0bc3e779f0bc084bdf194579f67
Victor Umoru <umoruvictor98@gmail.com>
2025-08-18 11:09:39 +0100

    Resume feedback components

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -60,6 +60,7 @@ const upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis compleete, redirecting...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     } 
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
